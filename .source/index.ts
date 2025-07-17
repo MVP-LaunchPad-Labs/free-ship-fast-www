3b82ae88c92fcee426f32b0ba838c9c8fb813437
Harsh-launchpad <debuggerbabu007@gmail.com>
2025-07-18 03:09:49 +0530

    feat: add fumadocs

---
+++ .source/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck -- skip type checking
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1752787085012"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/ROHIT SINGH RAWAT/OneDrive/Desktop/WORK/MVPs/free-ship-fast-www/content/docs/index.mdx"}, data: docs_0 }], [])
